ef6da228dbe21c23e885fd017ef63f929aeb4653
Siyuan <392140045@qq.com>
2017-05-01 15:50:32 +0800

    c

--- MapViewer/Rendering/Lighting.cpp
+++ MapViewer/Rendering/Lighting.cpp
@@ -6,6 +6,9 @@ namespace Rendering
 
 	LightBase::LightBase() :m_type(NONE),
 		m_intensity(1.0f),
+		m_constant(0.2f),
+		m_linear(0.03f),
+		m_quadratic(0.010),
 		m_isActivated(true),
 		m_diffuse(glm::vec3(1.0f, 1.0f, 1.0f)),
 		m_specular(glm::vec3(1.0f, 1.0f, 1.0f))
